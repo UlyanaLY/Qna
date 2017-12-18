e26ca35f9f5048e24a65faac6e35cc36e8456d79
UlyanaLY <oulyana@gmail.com>
2017-12-18 18:44:13 +0300

    TDD/BDD destroy

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,5 @@
 class Answer < ApplicationRecord
-  belongs_to :question
+  belongs_to :question, dependent: :destroy, optional: true
+
+  validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216052532) do
+ActiveRecord::Schema.define(version: 20171215201635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "answers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "question_id"
-    t.index ["question_id"], name: "index_answers_on_question_id"
-  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
