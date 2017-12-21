cd8e64f56b6db6e485bf35ae50fe41fe7801d401
UlyanaLY <oulyana@gmail.com>
2017-12-21 20:24:19 +0300

    Tests for controllers

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer, question: question) }
+
+  describe 'POST #create' do
+    let(:create_answer) { post :create, params: { question_id: question, answer: attributes_for(:answer) } }
+    context 'with valid attributes' do
+      it 'saves the new answer in the database' do
+        expect { create_answer }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirects to show question view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) } }.to_not change(Answer, :count)
+      end
+
+      it 'redirects to show question view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question, answer: attributes_for(:answer) } }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'redirects to show question view' do
+      post :create, params: { question_id: question, answer: attributes_for(:answer) }
+      expect(response).to redirect_to question_path(assigns(:question))
+    end
+  end
+end
