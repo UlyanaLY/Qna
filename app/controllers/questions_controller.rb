f67eace33fcee8bf56c8dcc40a4713170c03bb5f
UlyanaLY <oulyana@gmail.com>
2018-01-28 20:58:37 +0300

    Add files to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.attachments.build
   end
 
   def edit; end
@@ -58,7 +59,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, attachments_attributes: [:file])
   end
 
   def verify_user
