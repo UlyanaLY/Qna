613cb141a4310e7ac7f782941df8ffb279434d85
Ulyana Lyzhina <you@example.com>
2018-05-06 17:29:35 +0300

    CanCan for profiles controller

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::ProfilesController < ApplicationController
   respond_to :json
 
   before_action :doorkeeper_authorize!
+
+  authorize_resource class: User
+
   def me
     respond_with current_resource_owner
   end
@@ -9,5 +12,9 @@ class Api::V1::ProfilesController < ApplicationController
   protected
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def current_ability
+    @ability ||= Ability.new(current_resource_owner)
   end
 end
