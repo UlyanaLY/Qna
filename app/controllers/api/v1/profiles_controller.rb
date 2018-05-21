a1fdadd9a13689de5f834db13651c66949e4fedc
Ulyana Lyzhina <you@example.com>
2018-05-21 19:13:29 +0300

    Fixed

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::ProfilesController < ApplicationController
 
   before_action :doorkeeper_authorize!
 
+  authorize_resource class: User
+
   def me
     respond_with current_resource_owner
   end
